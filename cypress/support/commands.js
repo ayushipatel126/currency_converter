deab36ecb5cf77ceea2446747958ba71003964ad
Ayushi Patel <ayushipatel126@gmail.com>
2020-12-13 15:17:28 -0500

    added check for conversion rate and 4 decimal output value

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,18 +11,34 @@ Cypress.Commands.add('convertCurrency', (from, to, value, date = null) => {
     cy.get('[data-cy=submit]').click()
 })
 
+function getConversionRate(from, to, date) {
+    return cy.request({
+        method: 'GET',
+        url: `https://www.bankofcanada.ca/valet/observations/FX${from}${to}?start_date=${date.format('YYYY-MM-DD')}&end_date=${date.format('YYYY-MM-DD')}`
+    })
+}
+
 Cypress.Commands.add('verifyConvertedOutput', (from, to, value, date = null) => {
-    var dateObj = date ? moment(date) : moment();
-    var isWeekend = (dateObj.day() === 6) || (dateObj.day() === 0);
+    date = date ? moment(date) : moment();
+    var isWeekend = (date.day() === 6) || (date.day() === 0);
     if (isWeekend) {
         cy.get('[data-cy="error"]')
             .should('contain', 'Error: No observation found for specific date')
     } else {
-        cy.get('[data-cy="output"]')
-            .should('contain', `From: ${from}`)
-            .should('contain', `To: ${to}`)
-            .should('contain', `Date: ${dateObj.format('YYYY-MM-DD')}`)
-            .should('contain', `Amount: ${value}`)
-            .should('contain', `Series: FX${from}${to}`)         
+        getConversionRate(from, to, date).then((response) => {
+            console.log(response.body.observations[0][`FX${from}${to}`]);
+            expect(response.status).to.eq(200)
+            expect(response.body.observations[0]).to.have.property(`FX${from}${to}`)
+            var conversionRate = response.body.observations[0][`FX${from}${to}`].v
+            var calculatedAmount = conversionRate * value
+            cy.get('[data-cy="output"]')
+                .should('contain', `From: ${from}`)
+                .should('contain', `To: ${to}`)
+                .should('contain', `Date: ${date.format('YYYY-MM-DD')}`)
+                .should('contain', `Amount: ${value}`)
+                .should('contain', `Series: FX${from}${to}`)
+                .should('contain', `1 ${from} = ${conversionRate} ${to}`)
+                .should('contain', `Final Value: ${calculatedAmount.toFixed(4)}`)    
+        });
     }
 })
