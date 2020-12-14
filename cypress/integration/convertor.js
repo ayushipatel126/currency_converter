e5c9bcf9b69807cdbfaf01d723eb022523505de6
Ayushi Patel <ayushipatel126@gmail.com>
2020-12-13 19:22:37 -0500

    comment added

--- cypress/integration/convertor.js
+++ cypress/integration/convertor.js
@@ -1,73 +1,75 @@
 describe("Currency Convertor", () => {
 
     let currencies = [];
+
     before(() => {
         cy.fixture('foreignCurrencies')
             .then((foreignCurrencies) => {
-                console.log(foreignCurrencies);
-                currencies = foreignCurrencies
+                currencies = foreignCurrencies;
             })
-
     })
+
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('/');
     })
 
+    // Convert foreign currencies to canadian dollars based on currency code
     it("should convert foreign currencies to canadian dollars with date-weekday", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency(currency, 'CAD', 5, '2020-12-11')
-            cy.verifyConvertedOutput(currency, 'CAD', 5, '2020-12-11')
+            cy.convertCurrency(currency, 'CAD', 5, '2020-12-11');
+            cy.verifyConvertedOutput(currency, 'CAD', 5, '2020-12-11');
         });
     })
 
     it("should convert foreign currencies to canadian dollars with date-weekend", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency(currency, 'CAD', 5, '2020-12-12')
-            cy.verifyConvertedOutput(currency, 'CAD', 5, '2020-12-12')
+            cy.convertCurrency(currency, 'CAD', 5, '2020-12-12');
+            cy.verifyConvertedOutput(currency, 'CAD', 5, '2020-12-12');
         });
     })
 
     it("should convert foreign currencies to canadian dollars with date-holiday", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency(currency, 'CAD', 5, '2020-01-01')
+            cy.convertCurrency(currency, 'CAD', 5, '2020-01-01');
             cy.get('[data-cy="error"]')
-            .should('contain', 'Error: No observation found for specific date')
+            .should('contain', 'Error: No observation found for specific date');
         });
     })
 
     it("should convert foreign currencies to canadian dollars without date", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency(currency, 'CAD', 5)
-            cy.verifyConvertedOutput(currency, 'CAD', 5)
+            cy.convertCurrency(currency, 'CAD', 5);
+            cy.verifyConvertedOutput(currency, 'CAD', 5);
         });
     })
 
+    // Convert canadain dollars to foreign currencies based on currency code
     it("should convert canadian dollars to foreign currencies with date-weekday", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency('CAD', currency, 5, '2020-12-11')
-            cy.verifyConvertedOutput('CAD', currency, 5, '2020-12-11')
+            cy.convertCurrency('CAD', currency, 5, '2020-12-11');
+            cy.verifyConvertedOutput('CAD', currency, 5, '2020-12-11');
         });
     })
 
     it("should convert canadian dollars to foreign currencies with date-weekend", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency('CAD', currency, 5, '2020-12-12')
-            cy.verifyConvertedOutput('CAD', currency, 5, '2020-12-12')
+            cy.convertCurrency('CAD', currency, 5, '2020-12-12');
+            cy.verifyConvertedOutput('CAD', currency, 5, '2020-12-12');
         });
     })
 
     it("should convert canadian dollars to foreign currencies with date-holiday", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency('CAD', currency, 5, '2020-01-01')
+            cy.convertCurrency('CAD', currency, 5, '2020-01-01');
             cy.get('[data-cy="error"]')
-            .should('contain', 'Error: No observation found for specific date')
+            .should('contain', 'Error: No observation found for specific date');
         });
     })
 
     it("should convert canadian dollars to foreign currencies without date", () => {
         currencies.forEach(currency => {
-            cy.convertCurrency('CAD', currency, 5)
-            cy.verifyConvertedOutput('CAD', currency, 5)
+            cy.convertCurrency('CAD', currency, 5);
+            cy.verifyConvertedOutput('CAD', currency, 5);
         });
     })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,18 @@
 /// <reference types="Cypress" />
 import moment from 'moment';
 
+// Fill and submit the form based on given inputs
 Cypress.Commands.add('convertCurrency', (from, to, value, date = null) => {
-    cy.get('[data-cy=fromCurrency]').find('input').clear().type(from)
-    cy.get('[data-cy=toCurrency]').find('input').clear().type(to)
+    cy.get('[data-cy=fromCurrency]').find('input').clear().type(from);
+    cy.get('[data-cy=toCurrency]').find('input').clear().type(to);
     if (date) {
-        cy.get('[data-cy=date]').find('input').clear().type(date)
+        cy.get('[data-cy=date]').find('input').clear().type(date);
     }
-    cy.get('[data-cy=value]').find('input').clear().type(value)
-    cy.get('[data-cy=submit]').click()
+    cy.get('[data-cy=value]').find('input').clear().type(value);
+    cy.get('[data-cy=submit]').click();
 })
 
+// Get observations from Bank of Canada based on date
 function getConversionRate(from, to, date) {
     return cy.request({
         method: 'GET',
@@ -18,19 +20,20 @@ function getConversionRate(from, to, date) {
     })
 }
 
+// Verify converted output (including conversion rate given by Bank of Canada) based on weekend or weekdays
+// Output should display date, conversion rate and output value with 4 decimal digits 
 Cypress.Commands.add('verifyConvertedOutput', (from, to, value, date = null) => {
     date = date ? moment(date) : moment();
     var isWeekend = (date.day() === 6) || (date.day() === 0);
     if (isWeekend) {
         cy.get('[data-cy="error"]')
-            .should('contain', 'Error: No observation found for specific date')
+            .should('contain', 'Error: No observation found for specific date');
     } else {
         getConversionRate(from, to, date).then((response) => {
-            console.log(response.body.observations[0][`FX${from}${to}`]);
-            expect(response.status).to.eq(200)
-            expect(response.body.observations[0]).to.have.property(`FX${from}${to}`)
-            var conversionRate = response.body.observations[0][`FX${from}${to}`].v
-            var calculatedAmount = conversionRate * value
+            expect(response.status).to.eq(200);
+            expect(response.body.observations[0]).to.have.property(`FX${from}${to}`);
+            var conversionRate = response.body.observations[0][`FX${from}${to}`].v;
+            var calculatedAmount = conversionRate * value;
             cy.get('[data-cy="output"]')
                 .should('contain', `From: ${from}`)
                 .should('contain', `To: ${to}`)
@@ -38,7 +41,7 @@ Cypress.Commands.add('verifyConvertedOutput', (from, to, value, date = null) => 
                 .should('contain', `Amount: ${value}`)
                 .should('contain', `Series: FX${from}${to}`)
                 .should('contain', `1 ${from} = ${conversionRate} ${to}`)
-                .should('contain', `Final Value: ${calculatedAmount.toFixed(4)}`)    
+                .should('contain', `Final Value: ${calculatedAmount.toFixed(4)}`);    
         });
     }
 })
